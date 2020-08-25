52152bc67b06103177b948325bb95e1ea61714c1
Charles Engelke <engelke@google.com>
2020-08-24 17:44:52 -0700

    Skeletal public and employee sites

--- support/main.py
+++ support/main.py
@@ -15,13 +15,32 @@
 import os
 
 from flask import Flask, make_response, render_template, request
+from google.cloud import firestore
 
 
 app = Flask(__name__)
 
 
+def get_email():
+    authenticated_email = request.headers.get('X-Goog-Authenticated-User-Email', '')
+    if authenticated_email.startswith('accounts.google.com:'):
+        return authenticated_email[20:]
+    else:
+        return authenticated_email
+
+
+def list_requests():
+    db = firestore.Client()
+    docs = db.collection('requests').where('email', '==', get_email()).stream()
+    results = []
+    for doc in docs:
+        results.append(doc.to_dict())
+    return results
+
+
 @app.route('/', methods=['GET'])
 def home_page():
-    support_url = os.environ.get('support')
-    return render_template('homepage.html', support=support_url)
+    public_url = os.environ.get('public')
+    return render_template('homepage.html', public=public_url,
+        requests=list_requests())
     
